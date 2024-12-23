7ac97ca7390d1785a11b15db1cc7c7a8e9cbda02
scarqin <scarqin@gmail.com>
2024-12-23 17:45:50 +0800

    chore:  文件拆分

---
+++ frontend/packages/core/src/pages/playground/components/ServiceCardNode.tsx
@@ -0,0 +1,15 @@
+import { Icon } from '@iconify/react'
+import { Handle, NodeProps, Position } from '@xyflow/react'
+import React from 'react'
+
+export const ServiceCardNode: React.FC<NodeProps> = () => {
+  return (
+    <div className="node-card bg-white rounded-lg shadow-sm p-4 min-w-[150px] relative">
+      <Handle type="source" position={Position.Right} />
+      <div className="flex flex-col gap-2 items-center">
+        <Icon icon="mdi:robot" className="text-3xl text-[--primary-color]" />
+        <span className="text-base text-gray-900">AI Service</span>
+      </div>
+    </div>
+  )
+}
