243e1da7168e38f5f417c78a296fd0dd8f61f547
scarqin <scarqin@gmail.com>
2024-12-24 11:35:31 +0800

    feat: dmo

--- frontend/packages/core/src/pages/playground/components/types.ts
+++ frontend/packages/core/src/pages/playground/components/types.ts
@@ -1,9 +1,20 @@
-export type ModelCardStatus = 'success' | 'failure'
+export type ModelStatus = 'enable' | 'abnormal'|'disable'
+export type KeyStatus ='normal' | 'abnormal'|'disable'
+
+export interface KeyData {
+  id: string
+  name: string
+  status: KeyStatus,
+}
 
 export interface ModelData {
   id: string
-  type: string
-  title: string
-  status: ModelCardStatus
-  defaultModel: string
+  name: string
+  logo: string
+  default_llm: string
+  status: ModelStatus
+  api_count: number
+  key_count: number
+  keys: KeyData[]
+  priority?: number
 }
