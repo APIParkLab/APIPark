9971a86d5c34b5bcffac637afa899e6d0616c9cd
scarqin <scarqin@gmail.com>
2025-01-03 09:09:40 +0800

    chore: apis

---
+++ frontend/packages/core/src/pages/aiApis/types.ts
@@ -0,0 +1,11 @@
+export interface APIs {
+    id: string;
+    name: string;
+    service: string;
+    method: string;
+    request_path: string;
+    model: string;
+    disabled: boolean;
+    update_time: string;
+    use_token: number;
+}
