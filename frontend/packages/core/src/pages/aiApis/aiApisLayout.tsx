29c74bbbe196fe74dd38aa08b6e1a113937fb5e6
scarqin <scarqin@gmail.com>
2025-01-07 17:56:16 +0800

    fix: jump link error

--- frontend/packages/core/src/pages/aiApis/aiApisLayout.tsx
+++ frontend/packages/core/src/pages/aiApis/aiApisLayout.tsx
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
-export default function GlobalPolicyLayout(){
-    const location = useLocation()
-    const pathName = location.pathname
-    const navigator = useNavigate()
-    useEffect(()=>{
-        if(pathName === '/aiApis'){
-            navigator('/aiApis/list')
-        }
-    },[pathName])
-    return (<Outlet></Outlet>)
+export default function GlobalPolicyLayout() {
+  const location = useLocation()
+  const pathName = location.pathname
+  const navigator = useNavigate()
+  useEffect(() => {
+    if (pathName === '/aiApis') {
+      const queryParams = new URLSearchParams(location.search).toString()
+      navigator(`/aiApis/list${queryParams ? `?${queryParams}` : ''}`)
+    }
+  }, [pathName])
+  return <Outlet></Outlet>
 }
