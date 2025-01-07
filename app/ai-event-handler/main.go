1d09965d9a05283b629aa4a41299015f9e3413d8
Liujian <824010343@qq.com>
2025-01-07 12:49:23 +0800

    AI API token quantity docking completed

---
+++ app/ai-event-handler/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/eolinker/go-common/autowire"
+	nsq "github.com/nsqio/go-nsq"
+
+	"github.com/eolinker/go-common/cftool"
+
+	_ "github.com/eolinker/go-common/store/store_mysql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	version  string
+	confPath string
+)
+
+func init() {
+	flag.StringVar(&confPath, "c", "config.yml", "`config` file path for server ")
+}
+
+type ServerConfig struct {
+	Port int `yaml:"port"`
+}
+
+func main() {
+	// 1. 连接 MySQL 数据库
+	cftool.Register[ServerConfig](fmt.Sprintf("root:%s", confPath))
+	cftool.ReadFile(confPath)
+
+	handler := &NSQHandler{}
+	autowire.Autowired(handler)
+	err := autowire.CheckComplete()
+	if err != nil {
+		log.Fatal("check autowired:", err)
+		return
+	}
+
+	// 2. 创建 NSQ 消费者
+	config := nsq.NewConfig()
+	consumer, err := nsq.NewConsumer("ai_event", "tmp", config)
+	if err != nil {
+		log.Fatalf("Failed to create NSQ consumer: %v", err)
+	}
+
+	consumer.AddHandler(handler)
+
+	// 4. 连接到 NSQ
+	nsqAddress := "172.18.166.219:9150" // NSQ 地址
+	err = consumer.ConnectToNSQD(nsqAddress)
+	if err != nil {
+		log.Fatalf("Failed to connect to NSQ: %v", err)
+	}
+	log.Println("Connected to NSQ")
+
+	// 5. 捕获系统信号，优雅关闭
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	// 优雅停止消费者
+	consumer.Stop()
+	<-consumer.StopChan
+	log.Println("NSQ Consumer stopped")
+}
--- go.mod
+++ go.mod
@@ -11,8 +11,10 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.4
 	github.com/getkin/kin-openapi v0.127.0
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-sql-driver/mysql v1.7.0
 	github.com/google/uuid v1.6.0
 	github.com/influxdata/influxdb-client-go/v2 v2.14.0
+	github.com/nsqio/go-nsq v1.1.0
 	github.com/urfave/cli v1.22.16
 	golang.org/x/crypto v0.24.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -37,8 +39,8 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
