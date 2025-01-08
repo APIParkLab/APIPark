b77ce23b3982d1e01257122dc941650bce739887
scarqin <scarqin@gmail.com>
2025-01-08 15:13:37 +0800

    fix: System Settings - General After changing the interface language, the internal pages do not automatically follow the language switch

--- frontend/packages/core/src/pages/common/CommonPage.tsx
+++ frontend/packages/core/src/pages/common/CommonPage.tsx
@@ -1,33 +1,26 @@
-import InsidePage from "@common/components/aoplatform/InsidePage";
-import { $t } from "@common/locales";
-import ServiceCategory from "./ServiceCategory";
-import ApiRequestSetting from "./ApiRequestSetting";
-import { Row, Col } from "antd";
+import InsidePage from '@common/components/aoplatform/InsidePage'
+import { useI18n } from '@common/locales'
+import { Col, Row } from 'antd'
+import ApiRequestSetting from './ApiRequestSetting'
+import ServiceCategory from './ServiceCategory'
 
-export default function CommonPage(){
-    return (
-        <InsidePage 
-                pageTitle={$t('常规设置')} 
-                showBorder={false}
-                contentClassName="pr-PAGE_INSIDE_X"
-                scrollPage={false}
-                >
-                    <Row className="mb-btnybase" >
-                         <Col  >
-                         <span className="font-bold mr-[13px]">
-                            {$t('API 请求设置')}
-                            </span>
-                        </Col>
-                    </Row>
-                    <ApiRequestSetting />
-                    <Row className="mb-btnybase mt-[40px]">
-                        <Col  >
-                        <span className="font-bold mr-[13px]">
-                            {$t('服务分类')}
-                        </span>
-                        </Col>
-                    </Row>
-                    <ServiceCategory />
-        </InsidePage>
-    )
+export default function CommonPage() {
+  const $t = useI18n()
+
+  return (
+    <InsidePage pageTitle={$t('常规设置')} showBorder={false} contentClassName="pr-PAGE_INSIDE_X" scrollPage={false}>
+      <Row className="mb-btnybase">
+        <Col>
+          <span className="font-bold mr-[13px]">{$t('API 请求设置')}</span>
+        </Col>
+      </Row>
+      <ApiRequestSetting />
+      <Row className="mb-btnybase mt-[40px]">
+        <Col>
+          <span className="font-bold mr-[13px]">{$t('服务分类')}</span>
+        </Col>
+      </Row>
+      <ServiceCategory />
+    </InsidePage>
+  )
 }
